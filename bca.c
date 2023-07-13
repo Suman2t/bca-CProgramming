8424eb2876023611d85209aa80fb716cec207f37
techworld <loldna47@gmail.com>
2023-07-13 07:19:17 +0545

    Add new file

---
+++ bca.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+  int main();
+   {
+    int Add,Subtraction,Multiplication,Division;
+    int firstInput,secondInput;
+
+    printf("Enter the First Number: ",'\n');
+    scanf("%c"&firstInput);
+
+    printf("Enter the Second Number: ",'\n');
+    scanf("%c"&secondInput);
+
+    Add = firstInput + secondInput ;
+    printf("Sum = %c",Add);
+    return 0;
+   }
